87fbd275f45d6c6101c7db44eff8c607225d4553
Murilo Pupo de Oliveira <48927673+mpupo@users.noreply.github.com>
2020-05-01 02:13:37 -0300

    Alteração no código devido a troca do nome do json

--- cesar-cryptography/init_challenge.py
+++ cesar-cryptography/init_challenge.py
@@ -10,7 +10,7 @@ MY_TOKEN = '11ff04314da6eaf77a2f5a853fab2c97a1bd3c9d'
 payload = {'token': MY_TOKEN}
 start_req = requests.get(CODENATION_START_CHALLENGE_URL, params=payload)
 
-with open('answer.json', 'w') as json_file:
+with open('answer_original.json', 'w') as json_file:
     challenge_params = start_req.json()
     json.dump(challenge_params, json_file)
 
